2a4897c6656014a88c41151ffd2e8a71c81cb185
imazan00 <i.mazan@innopolis.ru>
2018-03-10 23:36:36 +0300

    First commit

---
+++ src/main/java/com/example/demo/TeaMarketApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaMarketApplication.class, args);
+	}
+}
